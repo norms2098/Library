8936b4565994d79a8894f540ccdab6f372381812
norms2098 <nrmsdgdgn@gmail.com>
2023-02-19 22:37:54 +0800

    fixed sidebar spacing.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function bookInfo(title,author,pages,readStatus){
+/*function bookInfo(title,author,pages,readStatus){
     this.bookTitle=title;
     this.bookAuthor=author;
     this.numofPages=pages;
@@ -15,4 +15,11 @@ function bookInfo(title,author,pages,readStatus){
 
 const bookOne= new bookInfo('The Hobbit','J.R.R. Tolkien','295',true);
 
-console.log(bookOne.info);
+console.log(bookOne.info);*/
+
+const grid = document.querySelector('.grid-container');
+const sidebarMinimizeBtn = document.querySelector('.material-symbols-outlined');
+
+sidebarMinimizeBtn.addEventListener('click',() =>{
+    grid.style.gridTemplateColumns="85vw 10vw"
+})
