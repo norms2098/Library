953233be1ad30b81ab0fc1de10060d96faa87103
norms2098 <nrmsdgdgn@gmail.com>
2023-03-04 19:14:25 +0800

    Added working display to table function

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-/*function bookInfo(title,author,pages,readStatus){
-    this.bookTitle=title;
-    this.bookAuthor=author;
-    this.numofPages=pages;
-    if(!readStatus){
-        this.isRead='Not yet read';
-    }
-    else{
-        this.isRead='already read';
-    }
-    
-
-    this.info = `${this.bookTitle} is by ${this.bookAuthor}, ${this.numofPages} pages, ${this.isRead}.`;
-}
-
-const bookOne= new bookInfo('The Hobbit','J.R.R. Tolkien','295',true);
-
-console.log(bookOne.info);*/
-
 const grid = document.querySelector('.grid-container');
 const sidebarMinimizeBtn = document.querySelector('.material-symbols-outlined');
 
@@ -25,19 +6,15 @@ sidebarMinimizeBtn.addEventListener('click',() =>{
     grid.style.gridTemplateColumns="85vw 10vw"
 })
 
-const titleBook = document.getElementById('book-title');
-const authorBook = document.getElementById('book-author');
-const pagesBook = document.getElementById('book-pages');
+const bookForm = document.getElementById('book-form')
 const checkBox = document.getElementById('isRead');
-const submitBtn = document.getElementById('submit-button');
-
-const tableOutput = document.querySelector('.output-table');
+const tableOutput = document.getElementById('lib-table');
 
 let bookStatus="not Read";
-
-
 let myLibrary=[];
-
+let removeBtn = document.createElement("button");
+removeBtn.innerHTML="Remove book"
+let addRemoveBtn = document.tableOutput.appendChild(removeBtn)
 function Book(title, author,pages,readStatus){
     this.bookTitle = title;
     this.bookAuthor = author;
@@ -46,23 +23,25 @@ function Book(title, author,pages,readStatus){
 }
 
 function addBooktoLibrary(){
-    myLibrary.push(Book(titleBook.value,authorBook.value,pagesBook.value,bookStatus))
+    const titleBook = bookForm.elements['book-title'].value;
+    const authorBook = bookForm.elements['book-author'].value;
+    const pagesBook = bookForm.elements['book-pages'].value;
+    const newBook = new Book(titleBook,authorBook,pagesBook,bookStatus);
+    myLibrary.push(newBook)
+}
+
+function addBooktoTable(){
+    tableOutput.innerHTML += `<tr>
+    <td> ${myLibrary[myLibrary.length -  1 ].bookTitle} </td>
+    <td> ${myLibrary[myLibrary.length -  1 ].bookAuthor} </td>
+    <td> ${myLibrary[myLibrary.length -  1 ].bookPages} </td>
+    <td> ${myLibrary[myLibrary.length -  1 ].isBookRead} </td>
+    <td> ${addRemoveBtn}</td>
+    </tr>`
 }
 
 sidebarMinimizeBtn.addEventListener('click',() =>{
     grid.style.gridTemplateColumns="85vw 10vw"
-})
-submitBtn.addEventListener('click',() => {
-    addBooktoLibrary();
-    var row = tableOutput.insertRow(-1);
-    var bookTitle_table=row.insertCell(0);
-    var bookAuthor_table=row.insertCell(1);
-    var bookPages_table=row.insertCell(2);
-    var isBookRead_table=row.insertCell(3);
-    bookTitle_table.innerHTML=titleBook.value;
-    bookAuthor_table.innerHTML=authorBook.value;
-    bookPages_table.innerHTML=pagesBook.value;
-    isBookRead_table.innerHTML=bookStatus;
 })
 checkBox.addEventListener('change',() => {
     if(checkBox.checked){
@@ -71,6 +50,13 @@ checkBox.addEventListener('change',() => {
     else{
         bookStatus="not read";
     }
+})
+
+bookForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+    addBooktoLibrary();
+    addBooktoTable();
+    bookForm.reset(); 
 })
 
 
