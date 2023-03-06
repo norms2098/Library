5f61a63bf5003c2126fe0fcdf4a1060d9e7984ed
norms2098 <nrmsdgdgn@gmail.com>
2023-03-06 16:21:26 +0800

    fixed remove function

--- script.js
+++ script.js
@@ -8,20 +8,21 @@ sidebarMinimizeBtn.addEventListener('click',() =>{
 
 const bookForm = document.getElementById('book-form')
 const checkBox = document.getElementById('isRead');
-const tableOutput = document.getElementById('lib-table');
+const tableWhole = document.getElementById('lib-table');
 
-let bookStatus="not Read";
+let bookStatus="Not Read";
 let myLibrary=[];
+let headers = ["Title","Author","Pages","Status"];
 /*let removeBtn = document.createElement("button");
 removeBtn.innerHTML="Remove book"
 let editReadStatBtn = document.createElement("button");
 editReadStatBtn.innerHTML="Edit Read Status"*/
 
 function Book(title, author,pages,readStatus){
-    this.bookTitle = title;
-    this.bookAuthor = author;
-    this.bookPages = pages;
-    this.isBookRead = readStatus;
+    this.Title = title;
+    this.Author = author;
+    this.Pages = pages;
+    this.Status = readStatus;
 }
 
 function addBooktoLibrary(){
@@ -32,7 +33,7 @@ function addBooktoLibrary(){
     myLibrary.push(newBook)
 }
 
-function addBooktoTable(){
+/*function addBooktoTable(){
     const newRow = document.createElement("tr");
     for(let i=0;i<5;i++){
         const cell = document.createElement("td");
@@ -50,11 +51,11 @@ function addBooktoTable(){
             cell.appendChild(cellContent);
         }
         if(i==3){
-    
             const statbutton = document.createElement("button")
             statbutton.innerHTML = myLibrary[myLibrary.length -  1 ].isBookRead;
             statbutton.className = "changeStatus-btn";
             cell.appendChild(statbutton);
+
             statbutton.addEventListener('click',()=>{
                 if(statbutton.innerHTML==="Read"){
                     statbutton.innerHTML = "Not Read"
@@ -70,17 +71,59 @@ function addBooktoTable(){
             rembtn.innerHTML = "Remove Book";
             rembtn.className = "removeBook-btn";
             cell.appendChild(rembtn);
+            rembtn.addEventListener('click',()=>{
+                rembtn.closest("tr").remove();
+                myLibrary.splice(i,1);
+                console.log(myLibrary)
+            })
         }
         
         newRow.appendChild(cell);
     }
     tableOutput.appendChild(newRow)
+}*/
+
+function addBooktoTable(){
+    if(document.getElementById('lib-content')){
+        document.getElementById('lib-content').remove();
+    }
+    let newTableContent = document.createElement("tbody");
+    newTableContent.id="lib-content";
+
+    for(let i=0;i<= myLibrary.length;i++){
+        let newRow = document.createElement("tr");
+        
+        headers.forEach(function(header,index){
+            let bookCell = document.createElement("td");
+            bookCell.innerHTML = myLibrary[i][header];
+            bookCell.classList.add("box"+ (index + 1));
+            newRow.appendChild(bookCell);
+        })
+
+        let remBtnCell = document.createElement("td");
+        let remBtn = document.createElement("button");
+        remBtn.innerHTML = "Remove Book";
+        remBtn.classList.add("remove-btn");
+        remBtn.addEventListener("click", () =>{
+            myLibrary.splice(i,1);
+            remBtn.closest("tr").remove();
+            console.log(myLibrary);
+        })
+
+        remBtnCell.appendChild(remBtn);
+        newRow.appendChild(remBtnCell);
+        newTableContent.appendChild(newRow);
+        tableWhole.appendChild(newTableContent);
+     
+    }
+    
 }
     
 
 sidebarMinimizeBtn.addEventListener('click',() =>{
     grid.style.gridTemplateColumns="85vw 10vw"
 })
+
 checkBox.addEventListener('change',() => {
     if(checkBox.checked){
         bookStatus="Read";
@@ -93,18 +136,10 @@ checkBox.addEventListener('change',() => {
 bookForm.addEventListener('submit',(e) => {
     e.preventDefault();
     addBooktoLibrary();
-    addBooktoTable();
     bookForm.reset(); 
+    addBooktoTable();
+    
     bookStatus="Not Read";
 })
 
-function changeStatus(classname,status){
-    const changestat = document.querySelector(`.${classname}`)
-    if(status === "Read"){
-        changestat.innerHTML = "Not Read";
-    }
-    else{
-        changestat.innerHTML = "Read";
-    }
-}
 
